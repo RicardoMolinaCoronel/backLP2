12c628a7c59a52231a5d7f3a0b22d70f5d63788b
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2023-08-18 16:55:40 -0500

    update bd

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var usuarioRouter = require('./routes/rest_usuario');
 
 var app = express();
 
-// view engine setup
+//  view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 

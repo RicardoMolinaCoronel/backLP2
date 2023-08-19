61994f40ad0042b93b574237f34caee2e5493459
wayared <jarecast@espol.edu.ec>
2023-08-18 19:03:42 -0500

    evento api added

--- routes/rest_evento.js
+++ routes/rest_evento.js
@@ -17,7 +17,7 @@ router.get('/findAll/json', function(req, res, next) {
 
     });
 
-    
+
 	  router.get('/findById/:id/json', function(req, res, next) {  
       let id = req.params.id;
       evento.findByPk(id)
@@ -30,6 +30,66 @@ router.get('/findAll/json', function(req, res, next) {
           })
           .catch(error => res.status(400).send(error))
     });	
+
+
+    router.get('/findByDate/:date/json', function(req, res, next) {
+      let date = req.params.date;
+           
+      evento.findAll({
+        where: {
+          fecha: date // Filtrar por la fecha proporcionada en la URL
+        }
+      })
+      .then(eventos => {
+        if (eventos.length > 0) {
+          res.status(200).json(eventos);
+        } else {
+          res.status(404).json({ error: "No existen eventos para la fecha " + date });
+        }
+      })
+      .catch(error => res.status(400).send(error));
+    });
+
+    
+    router.get('/findByDateRange/:startDate/:endDate/json', function(req, res, next) {
+      let startDate = req.params.startDate;
+      let endDate = req.params.endDate;
+      
+      evento.findAll({
+        where: {
+          fecha: {
+            [Op.between]: [startDate, endDate]
+          }
+        }
+      })
+      .then(eventos => {
+        if (eventos.length > 0) {
+          res.status(200).json(eventos);
+        } else {
+          res.status(404).json({ error: "No existen eventos en el rango de fechas proporcionado" });
+        }
+      })
+      .catch(error => res.status(400).send(error));
+    });
+
+
+    router.get('/findByLocation/:location/json', function(req, res, next) {
+      let location = req.params.location;
+
+      evento.findAll({
+        where: {
+          ubicacion: location
+        }
+      })
+      .then(eventos => {
+        if (eventos.length > 0) {
+          res.status(200).json(eventos);
+        } else {
+          res.status(404).json({ error: "No existen eventos en la ubicación proporcionada" });
+        }
+      })
+      .catch(error => res.status(400).send(error));
+    });
 
 
       router.post('/save', function(req, res, next) {  

0507fc0d32688f4dae427a3dc242f7b329a0f4ba
fredtenesaca <fregtene@espol.edu.ec>
2023-08-20 23:44:48 -0500

    like y comentario peticiones

--- routes/rest_like.js
+++ routes/rest_like.js
@@ -2,12 +2,12 @@ var express = require('express');
   var router = express.Router();
 
   /* IMPORTE El ARCHIVO CON EL NOMBRE_CLASE */
-  const publicacion = require('../models').publicacion;
+  const like= require('../models').like;
 router.get('/findAll/json', function(req, res, next) {  
 
     	/* MÉTODO ESTÁTICO findAll  */
-
-      publicacion.findAll({  
+      //comentario
+      like.findAll({  
           attributes: { exclude: ["updatedAt", "createdAt"] } ,
       })  
       .then(resultado => {  
@@ -15,6 +15,22 @@ router.get('/findAll/json', function(req, res, next) {
       })  
       .catch(error => res.status(400).send(error)) 
 
+    });
+
+    //GET ID
+    router.get('/findById/:id/json', function(req, res, next) {  
+
+      let id = req.params.id;
+
+      like.findByPk(id)
+          .then(instancia => {
+            if(instancia) {
+              res.status(200).json(instancia);
+            } else {
+              res.status(404).json({error: "No existe registro con el identificador "+id})
+            }
+          })
+          .catch(error => res.status(400).send(error))
     });
 	  
     module.exports = router;

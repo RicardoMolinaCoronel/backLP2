d128dbb99cdaa7b794328f3ed6e577d00ca6a7d7
fredtenesaca <fregtene@espol.edu.ec>
2023-09-05 11:50:39 -0500

    get y post- likes y comentarios

--- routes/rest_comentario.js
+++ routes/rest_comentario.js
@@ -3,6 +3,7 @@ var express = require('express');
 
   /* IMPORTE El ARCHIVO CON EL NOMBRE_CLASE */
   const comentario= require('../models').comentario;
+  const { Op } = require("sequelize");
 router.get('/findAll/json', function(req, res, next) {  
 
     	/* MÉTODO ESTÁTICO findAll  */
@@ -32,5 +33,42 @@ router.get('/findAll/json', function(req, res, next) {
           })
           .catch(error => res.status(400).send(error))
     });
+      
+      router.post('/save', function(req, res, next) {  
+
+        comentario.create(req.body)
+          .then(instancia => {
+            res.status(201).json(instancia);
+          })
+          .catch(error => {
+            res.status(500).json({ error: 'Error al crear el registro' });
+          });
+
+      });
 	  
+
+router.put('/update/:id', function(req, res, next) {  
+
+        let id = req.params.id;
+
+        comentario.findByPk(id)
+          .then(instancia => {
+            if(instancia) {
+
+              instancia.update(req.body)
+                .then(instanciaActualizada => {
+                  res.status(201).json(instanciaActualizada);
+                })
+                .catch(error => {
+                  res.status(500).json({ error: 'Error al actualizar el registro' });
+                });
+
+            } else {
+              res.status(404).json({error: "No existe registro con el identificador "+id})
+            }
+          })
+          .catch(error => res.status(400).send(error))
+		  
+      });  
+  
     module.exports = router;
